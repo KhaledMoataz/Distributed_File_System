952c602c84ca46f95da719e9146f42cc55c4dd12
Khaled Nassar <khaled.nassar99@eng-st.cu.edu.eg>
2020-03-15 20:51:41 +0200

    Removed ID, communicate via ip only

--- datakeeper_core.py
+++ datakeeper_core.py
@@ -8,13 +8,12 @@ import parse
 # the master ip, master port -> connect to it, to receive replication requests (I'm the client)
 class Date_Keeper:
 
-    def __init__(self, server_ip, server_port, id):
+    def __init__(self, server_ip, server_port):
         self.context = None
         self.socket = None
         self.replicas_master_socket = None
         self.server_ip = server_ip
         self.server_port = server_port
-        self.id = id
 
     def establish_connection(self, master_ip, master_port):
         context = zmq.Context()
@@ -38,12 +37,12 @@ class Date_Keeper:
                 source_port = str(parsed_req[2])
                 socket.send_string('replicating file: {}.....'.format(file_name))
                 transfer.download_from_server(file_name, context, source_ip, source_port)
-                replicas_master_socket.send_string('{} {} {} {}'.format(file_name, id, server_port, port))
+                replicas_master_socket.send_string('{} {} {} {} {}'.format(file_name, source_ip, source_port, server_ip, server_port))
                 response = replicas_master_socket.recv_string()
                 print(response)
 
 
-def init_data_keeper_process(replicas_master_ip, replicas_master_port, server_ip, server_port, id):
-    data_keeper = Date_Keeper(server_ip, server_port, id)
+def init_data_keeper_process(server_ip, server_port, replicas_master_ip, replicas_master_port):
+    data_keeper = Date_Keeper(server_ip, server_port)
     data_keeper.establish_connection(replicas_master_ip, replicas_master_port)
     data_keeper.run()
